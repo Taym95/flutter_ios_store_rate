f2d2acafa940981100c493f075a2258a8d4e99e9
Bentaimia Haddadi <bentaimia.haddadi@signify.com>
2019-05-09 17:51:34 +0200

    Add author email

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_ios_store_rate/flutter_ios_store_rate.dart';
 
 void main() => runApp(MyApp());
