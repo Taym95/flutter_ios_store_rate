d38e4c1d82e74667df0a7c6292253d2b4c0e4de9
Bentaimia Haddadi <bentaimia.haddadi@signify.com>
2019-05-09 16:00:27 +0200

    initial commit

---
+++ ios/Classes/FlutterIosStoreRatePlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterIosStoreRatePlugin.h"
+
+@implementation FlutterIosStoreRatePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_ios_store_rate"
+            binaryMessenger:[registrar messenger]];
+  FlutterIosStoreRatePlugin* instance = [[FlutterIosStoreRatePlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
---
+++ lib/flutter_ios_store_rate.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class FlutterIosStoreRate {
+  static const MethodChannel _channel =
+      const MethodChannel('flutter_ios_store_rate');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version.replaceAll(new RegExp('iOS '), '');
+  }
+}
